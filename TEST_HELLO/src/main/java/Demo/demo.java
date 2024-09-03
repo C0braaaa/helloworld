92fa48144774a43dd4c059ec6ef410a47fdc1de9
C0braaaa <cobragaming0fo@gmail.com>
2024-09-03 18:15:52 +0700

    Add files via upload

---
+++ TEST_HELLO/src/main/java/Demo/demo.java
@@ -0,0 +1,22 @@
+package Demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class demo
+ */
+@WebServlet("/demo")
+public class demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().write("<h1>Hello World</h1>");
+	}
+}
